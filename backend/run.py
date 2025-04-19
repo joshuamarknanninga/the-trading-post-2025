b58d19949900245bd08077516a594a29526f727a
Joshua Nanninga <jmarknanninga@gmail.com>
2025-04-19 17:06:50 -0500

    commit back from a while

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-from app import create_app, db
+from app import create_app, db, socketio
 
 app = create_app()
 
@@ -6,4 +6,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
